2b9380d0c47213c26f1b57e22c4b4d56062f0d6c
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-06-15 14:05:20 +0200

    Opprettet Spring Boot-prosjekt

---
+++ src/test/kotlin/no/nav/navnosearchapi/NavnoSearchApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.navnosearchapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NavnoSearchApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
