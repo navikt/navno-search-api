d7d378a942117bf5501acfa8e2a0a9e5457a4c46
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-06-23 15:08:10 +0200

    Gjort pagesize konfigurerbar

--- src/main/kotlin/no/nav/navnosearchapi/service/SearchService.kt
+++ src/main/kotlin/no/nav/navnosearchapi/service/SearchService.kt
@@ -2,14 +2,15 @@ package no.nav.navnosearchapi.service
 
 import no.nav.navnosearchapi.model.Content
 import no.nav.navnosearchapi.respository.ContentRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 
 @Service
-class SearchService(val repository: ContentRepository) {
+class SearchService(val repository: ContentRepository, @Value("\${opensearch.page-size}") val pageSize: Int) {
     fun searchAllText(term: String, page: Int): Page<Content> {
-        val pageRequest = PageRequest.of(page, 10)
+        val pageRequest = PageRequest.of(page, pageSize)
 
         if (term.startsWith('"') && term.endsWith('"')) {
             return repository.searchAllTextForPhrase(term, pageRequest)
