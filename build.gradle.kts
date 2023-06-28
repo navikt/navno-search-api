2cb33f9db18d16573e85eb5c2da510cd41486022
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-06-28 12:46:39 +0200

    Implementert basic indeksering

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ repositories {
 }
 
 dependencies {
-	implementation("org.opensearch.client:spring-data-opensearch-starter:1.1.0")
-	implementation("org.opensearch.client:spring-data-opensearch-test-autoconfigure:1.1.0")
+	implementation("org.opensearch.client:spring-data-opensearch-starter:1.2.0")
+	implementation("org.opensearch.client:spring-data-opensearch-test-autoconfigure:1.2.0")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
