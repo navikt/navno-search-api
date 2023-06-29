d0e6e019485baa05b001b80b3f9b0277800627a8
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-06-29 12:43:15 +0200

    Fjernet endepunkt for å lagre enkelt dokument

--- src/main/kotlin/no/nav/navnosearchapi/controller/AdminController.kt
+++ src/main/kotlin/no/nav/navnosearchapi/controller/AdminController.kt
@@ -8,20 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AdminController(val service: AdminService) {
 
-    @PostMapping("/content/save")
-    fun saveContent(@RequestBody content: Content, @RequestHeader("app-name") appName: String): Content {
-        return service.saveContent(content, appName)
-    }
-
-    @PostMapping("/content/saveAll")
-    fun saveAllContent(@RequestBody content: List<Content>, @RequestHeader("app-name") appName: String): List<Content> {
+    @PostMapping("/content/{appName}")
+    fun saveContent(@RequestBody content: List<Content>, @PathVariable("appName") appName: String): List<Content> {
         return service.saveAllContent(content, appName)
     }
 
--- src/main/kotlin/no/nav/navnosearchapi/service/AdminService.kt
+++ src/main/kotlin/no/nav/navnosearchapi/service/AdminService.kt
@@ -16,10 +16,6 @@ class AdminService(
     val operations: ElasticsearchOperations,
     @Value("\${opensearch.page-size}") val pageSize: Int
 ) {
-    fun saveContent(content: Content, appName: String): Content {
-        return operations.save(content, indexCoordinates(appName))
-    }
-
     fun saveAllContent(content: List<Content>, appName: String): List<Content> {
         return operations.save(content, indexCoordinates(appName)).toList()
     }
