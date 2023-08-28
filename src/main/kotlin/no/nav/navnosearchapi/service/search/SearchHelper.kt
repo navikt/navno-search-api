21c0b702e52229ec6426d099fce779daf7bc9dfc
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-08-28 13:02:41 +0200

    Refactor, StringQuery -> NativeSearchQuery med WrapperQuery

--- src/main/kotlin/no/nav/navnosearchapi/service/search/SearchHelper.kt
+++ src/main/kotlin/no/nav/navnosearchapi/service/search/SearchHelper.kt
@@ -1,6 +1,8 @@
 package no.nav.navnosearchapi.service.search
 
 import no.nav.navnosearchapi.model.ContentDao
+import org.opensearch.data.client.orhlc.NativeSearchQueryBuilder
+import org.opensearch.index.query.WrapperQueryBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations
@@ -8,7 +10,6 @@ import org.springframework.data.elasticsearch.core.SearchHitSupport
 import org.springframework.data.elasticsearch.core.SearchHits
 import org.springframework.data.elasticsearch.core.SearchPage
 import org.springframework.data.elasticsearch.core.query.HighlightQuery
-import org.springframework.data.elasticsearch.core.query.StringQuery
 import org.springframework.data.elasticsearch.core.query.StringQueryBuilder
 import org.springframework.data.elasticsearch.core.query.highlight.Highlight
 import org.springframework.data.elasticsearch.core.query.highlight.HighlightField
@@ -26,17 +27,12 @@ class SearchHelper(
         highlightFields: List<HighlightField>
     ): SearchPage<ContentDao> {
         val pageRequest = PageRequest.of(page, pageSize)
-        val searchQuery = StringQuery(
-            if (filters != null) filteredQuery(query, filters) else query,
-            pageRequest
-        )
 
-        searchQuery.setHighlightQuery(
-            HighlightQuery(
-                Highlight(highlightFields),
-                ContentDao::class.java
-            )
-        )
+        val searchQuery = NativeSearchQueryBuilder()
+            .withQuery(WrapperQueryBuilder(if (filters != null) filteredQuery(query, filters) else query))
+            .withPageable(pageRequest)
+            .withHighlightQuery(highlightQuery(highlightFields))
+            .build()
 
         val searchHits = operations.search(searchQuery, ContentDao::class.java)
         return SearchHitSupport.searchPageFor(searchHits, pageRequest)
@@ -44,5 +40,12 @@ class SearchHelper(
 
     fun search(query: String, size: Int): SearchHits<ContentDao> {
         return operations.search(StringQueryBuilder(query).withMaxResults(size).build(), ContentDao::class.java)
+    }
+
+    private fun highlightQuery(highlightFields: List<HighlightField>): HighlightQuery {
+        return HighlightQuery(
+            Highlight(highlightFields),
+            ContentDao::class.java
+        )
     }
 }
