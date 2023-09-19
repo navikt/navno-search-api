b46891e1d62375f835aadbc46e29f9e7edda6c6e
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-09-19 15:59:45 +0200

    Todos

--- src/main/kotlin/no/nav/navnosearchapi/common/enums/ValidMetatags.kt
+++ src/main/kotlin/no/nav/navnosearchapi/common/enums/ValidMetatags.kt
@@ -1,8 +1,8 @@
 package no.nav.navnosearchapi.common.enums
 
 enum class ValidMetatags(override val descriptor: String) : DescriptorProvider {
-    INNHOLD("innhold"),
-    INFORMASJON("informasjon"),
+    INNHOLD("innhold"), //todo: fjerne
+    INFORMASJON("informasjon"), //todo: sette som default dersom ingen andre metatags, ingen fylker, ikke fil
     KONTOR("kontor"),
     SKJEMA("skjema"),
     NYHET("nyhet"),
