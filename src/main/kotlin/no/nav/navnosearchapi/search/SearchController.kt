bfebaaa25d700d8cba3f8e251e81417495ed35a2
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-09-28 13:39:44 +0200

    Revert "CORS fix"
    This reverts commit 0fe0390d87074900030d1e3fe093cd78051b987f.

--- src/main/kotlin/no/nav/navnosearchapi/search/SearchController.kt
+++ src/main/kotlin/no/nav/navnosearchapi/search/SearchController.kt
@@ -8,14 +8,12 @@ import no.nav.navnosearchapi.search.dto.ContentSearchPage
 import no.nav.navnosearchapi.search.service.SearchService
 import no.nav.navnosearchapi.search.service.search.Filter
 import org.springframework.data.domain.Sort
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin(origins = ["https://www.nav.no, https://www.ekstern.dev.nav.no"])
 class SearchController(val searchService: SearchService, val compatibilityService: CompatibilityService) {
 
     @GetMapping("/content/search")
