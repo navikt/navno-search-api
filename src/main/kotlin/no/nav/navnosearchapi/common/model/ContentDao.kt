ef3a74a4d849dcc2f6e80577d870ce4060c8e471
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-10-04 14:01:41 +0200

    Lagt inn synonym-filter med synonymer fra fil

--- src/main/kotlin/no/nav/navnosearchapi/common/model/ContentDao.kt
+++ src/main/kotlin/no/nav/navnosearchapi/common/model/ContentDao.kt
@@ -12,7 +12,7 @@ import org.springframework.data.elasticsearch.core.suggest.Completion
 import java.time.ZonedDateTime
 
 @Document(
-    indexName = "search-content-v6",
+    indexName = "search-content-v7",
     dynamic = Dynamic.STRICT,
     /* Disabler type hints da det lager et _class-felt i mappingen som gir problemer for wildcard-søk.
        Bør skrives om dersom vi trenger polymorfisk data. */
