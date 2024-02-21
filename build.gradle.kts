b3ef3569e80601c46c58bffed9a5c32f7e7e4a09
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-02-21 09:50:22 +0100

    Bruker siste versjon av index

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
         val opensearch = "1.3.0"
         val opensearchTestcontainers = "2.0.1"
         val testcontainers = "1.19.5"
-        val navnoSearchCommon = "20240214114843-1eeae1a"
+        val navnoSearchCommon = "20240221092424-e37515a"
     }
 
     implementation("no.nav.navnosearchadminapi:common:${versions.navnoSearchCommon}")
