4f7bf1df183063278d3c52a52d9552c5ca4c3ebe
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-03-05 13:30:42 +0100

    Versjonsnummer i variabler

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,22 +24,28 @@ repositories {
     mavenCentral()
 }
 
+val logstashVersion = "7.4"
+val opensearchVersion = "1.3.0"
+val opensearchTestcontainersVersion = "2.0.1"
+val testcontainersVersion = "1.19.6"
+val navnoSearchCommonVersion = "20240221092424-e37515a"
+
 dependencies {
-    implementation("no.nav.navnosearchadminapi:common:20240221092424-e37515a")
-    implementation("org.opensearch.client:spring-data-opensearch-starter:1.3.0") {
+    implementation("no.nav.navnosearchadminapi:common:$navnoSearchCommonVersion")
+    implementation("org.opensearch.client:spring-data-opensearch-starter:$opensearchVersion") {
         exclude("org.opensearch.client", "opensearch-rest-client-sniffer")
     }
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("org.opensearch.client:spring-data-opensearch-test-autoconfigure:1.3.0") {
+    testImplementation("org.opensearch.client:spring-data-opensearch-test-autoconfigure:$opensearchVersion") {
         exclude("org.opensearch.client", "opensearch-rest-client-sniffer")
     }
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:junit-jupiter:1.19.6")
-    testImplementation("org.opensearch:opensearch-testcontainers:2.0.1")
+    testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
+    testImplementation("org.opensearch:opensearch-testcontainers:$opensearchTestcontainersVersion")
 }
 
 tasks.withType<KotlinCompile> {
