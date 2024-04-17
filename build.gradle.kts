3b2603d24b82118946fc49dae6eeeeeed91c7f20
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-04-17 12:33:30 +0200

    Bumpet index-versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     val opensearchVersion = "1.3.0"
     val opensearchTestcontainersVersion = "2.0.1"
     val testcontainersVersion = "1.19.7"
-    val navnoSearchCommonVersion = "20240417113118-0e40234"
+    val navnoSearchCommonVersion = "20240417122509-fcff050"
 
     implementation("no.nav.navnosearchadminapi:common:$navnoSearchCommonVersion")
     implementation("org.opensearch.client:spring-data-opensearch-starter:$opensearchVersion") {
