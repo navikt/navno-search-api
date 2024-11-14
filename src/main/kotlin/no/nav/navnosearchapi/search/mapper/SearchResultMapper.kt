51ea45d4eb2bda3f89f98bab761ef1b97f2fdd9a
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-11-14 12:47:22 +0100

    Flyttet aggregation-mapping inn der den brukes

--- src/main/kotlin/no/nav/navnosearchapi/search/mapper/SearchResultMapper.kt
+++ src/main/kotlin/no/nav/navnosearchapi/search/mapper/SearchResultMapper.kt
@@ -4,8 +4,12 @@ import no.nav.navnosearchadminapi.common.enums.ValidAudiences
 import no.nav.navnosearchadminapi.common.enums.ValidMetatags
 import no.nav.navnosearchadminapi.common.model.Content
 import no.nav.navnosearchapi.search.controller.Params
+import no.nav.navnosearchapi.search.dto.Aggregations
+import no.nav.navnosearchapi.search.dto.FacetBucket
 import no.nav.navnosearchapi.search.dto.SearchHit
 import no.nav.navnosearchapi.search.dto.SearchResult
+import no.nav.navnosearchapi.search.dto.UnderAggregations
+import no.nav.navnosearchapi.search.filters.facets.fasettFilters
 import no.nav.navnosearchapi.search.utils.isInQuotes
 import org.opensearch.data.client.orhlc.OpenSearchAggregations
 import org.opensearch.search.aggregations.bucket.filter.Filter
@@ -91,4 +95,29 @@ private fun resolveTimestamps(
         showNoTimestamps -> Pair(null, null)
         else -> Pair(null, lastUpdated)
     }
+}
+
+private fun Map<String, Long>.toAggregations(params: Params) = Aggregations(
+    fasetter = UnderAggregations(
+        buckets = fasettFilters.map { facet ->
+            FacetBucket(
+                key = facet.key,
+                name = facet.name,
+                docCount = this[facet.name] ?: 0,
+                checked = facet.key == params.f,
+                underaggregeringer = facet.underFacets.map { underFacet ->
+                    FacetBucket(
+                        key = underFacet.key,
+                        name = underFacet.name,
+                        docCount = this[underFacet.aggregationName] ?: 0,
+                        checked = underFacet.key in params.uf,
+                    )
+                }.filterNotEmpty().let { UnderAggregations(it) }
+            )
+        }
+    )
+)
+
+private fun List<FacetBucket>.filterNotEmpty(): List<FacetBucket> {
+    return filter { b -> b.docCount > 0 }
 }
