a419c690a2bab265115674cf029f429ebc3c4696
Tania Holst <holst.tania@gmail.com>
2025-07-07 12:45:37 +0200

    OpenSearchContainer update

--- src/test/kotlin/no/nav/navnosearchapi/integrationtests/config/OpensearchConfig.kt
+++ src/test/kotlin/no/nav/navnosearchapi/integrationtests/config/OpensearchConfig.kt
@@ -1,18 +1,17 @@
 package no.nav.navnosearchapi.integrationtests.config
 
-import org.opensearch.testcontainers.OpensearchContainer
+import org.opensearch.testcontainers.OpenSearchContainer
 import org.springframework.boot.test.context.TestConfiguration
 import java.time.Duration
 
 @TestConfiguration
 class OpensearchConfig {
     companion object {
-        private val opensearch: OpensearchContainer<*> = OpensearchContainer("opensearchproject/opensearch:2.11.1")
+        private val opensearch: OpenSearchContainer<*> = OpenSearchContainer("opensearchproject/opensearch:3.0.1")
             .withStartupAttempts(5)
             .withStartupTimeout(Duration.ofMinutes(2))
 
         init {
-            opensearch.start()
             System.setProperty("opensearch.uris", opensearch.httpHostAddress)
         }
     }
