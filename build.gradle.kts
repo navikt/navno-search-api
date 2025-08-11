16b405c7839e63ca5455b1b95e8f24997ff24bf0
Tale Prestmo <tale.prestmo@gmail.com>
2025-08-11 14:05:51 +0200

    upgrade lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     val opensearchVersion = "1.8.1"
     val opensearchTestcontainersVersion = "2.1.3"
     val testcontainersVersion = "1.21.3"
-    val navnoSearchCommonVersion = "20250811125230-25a2e9e"
+    val navnoSearchCommonVersion = "20250811135906-5834ff9"
     val kotestVersion = "5.9.1"
     val mockkVersion = "1.14.4"
 
