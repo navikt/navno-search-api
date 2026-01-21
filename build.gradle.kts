403da399c439d26b0df75d4d93a49bf66d58c6b6
Tania Holst <holst.tania@gmail.com>
2026-01-21 07:43:04 +0100

    :green_heart: La til build-and-deploy som en composite action

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,9 @@ dependencies {
 
     implementation("no.nav.navnosearchadminapi:common:$navnoSearchCommonVersion")
     implementation("org.opensearch.client:spring-data-opensearch-starter:$opensearchVersion") {
-        exclude("org.opensearch.client", "opensearch-rest-client-sniffer")
+        exclude(group = "org.springframework.boot", module = "spring-boot-data-elasticsearch")
+        exclude(group = "org.springframework.data", module = "spring-data-elasticsearch")
+        exclude(group = "org.opensearch.client", module = "opensearch-rest-client-sniffer")
     }
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
