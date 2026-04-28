673ab3cc349b7d8c5c543fd61dabe008e3d01f5d
Tania Holst <holst.tania@gmail.com>
2026-04-28 11:55:07 +0200

    fikset bygg

--- src/main/kotlin/no/nav/navnosearchapi/NavnoSearchApiApplication.kt
+++ src/main/kotlin/no/nav/navnosearchapi/NavnoSearchApiApplication.kt
@@ -1,12 +1,9 @@
 package no.nav.navnosearchapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
-@SpringBootApplication(exclude = [ElasticsearchDataAutoConfiguration::class])
-@EnableElasticsearchRepositories(basePackages = ["no.nav.navnosearchadminapi.common"])
+@SpringBootApplication
 class NavnoSearchApiApplication
 
 fun main(args: Array<String>) {
